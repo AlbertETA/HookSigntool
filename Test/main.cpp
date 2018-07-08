a72c7cb864569aacdcab95bf0f2a0aec8f8a3c90
Jemmy Wang <wangzihao12345678@126.com>
2018-07-08 13:13:58 +0800

    Load HookSigntool.dll automatically

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,5 +1,9 @@
 #include <windows.h>
 
 int main() {
+	MessageBoxW(NULL, L"First MessageBox", L"Test", NULL);
+
+	GetProcAddress(LoadLibraryW(L"HookSigntool.dll"), "attach");
+
 	MessageBoxW(NULL, L"Test", L"Test", NULL);
 }
