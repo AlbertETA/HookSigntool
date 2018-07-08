57722fa24fc8bcef10cbac38ac94651e43e52858
Jemmy Wang <wangzihao12345678@126.com>
2018-07-08 11:32:43 +0800

    Try Hook MessageBoxW

--- HookSigntool/main.cpp
+++ HookSigntool/main.cpp
@@ -1,4 +1,26 @@
+#pragma comment(lib, "detours.lib")
 #include <Windows.h>
+
+#include <stdlib.h>
+#include <detours.h>
+
+//保存函数原型
+static int (WINAPI *OldMesssageBoxW)(
+	_In_opt_ HWND hWnd,
+	_In_opt_ LPCWSTR lpText,
+	_In_opt_ LPCWSTR lpCaption,
+	_In_ UINT uType) = MessageBoxW;
+
+//改写函数
+static int WINAPI NewMessageBoxW(
+	_In_opt_ HWND hWnd,
+	_In_opt_ LPCWSTR lpText,
+	_In_opt_ LPCWSTR lpCaption,
+	_In_ UINT uType)
+{
+	return OldMesssageBoxW(NULL, L"new MessageBox", L"Please", MB_OK);
+
+}
 
 BOOL WINAPI DllMain(
 	_In_ HINSTANCE hinstDLL,
@@ -9,10 +31,11 @@ BOOL WINAPI DllMain(
 	if (fdwReason == DLL_PROCESS_ATTACH)
 	{
 		MessageBoxA(NULL, "Attaching", "Hook", NULL);
-	}
-	if (fdwReason == DLL_PROCESS_DETACH)
-	{
-		MessageBoxA(NULL, "Detaching", "Hook", NULL);
+		DetourTransactionBegin();
+		DetourUpdateThread(GetCurrentThread());
+		DetourAttach(&(PVOID&)OldMesssageBoxW, NewMessageBoxW);
+		DetourTransactionCommit();
+		MessageBoxA(NULL, "Attached", "Hook", NULL);
 	}
 	return 1;
 }
