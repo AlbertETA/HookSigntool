a8e9835c94ff735d92080f7dd40da505fda1f4cf
Jemmy Wang <wangzihao12345678@126.com>
2018-07-23 20:50:09 +0800

    Change Project Test to GetLocalTime

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,9 +1,13 @@
 #include <windows.h>
+#include <string.h>
+#include <stdio.h>
 
 int main() {
 	MessageBoxW(NULL, L"First MessageBox", L"Test", NULL);
 
 	GetProcAddress(LoadLibraryW(L"HookSigntool.dll"), "attach");
 
-	MessageBoxW(NULL, L"Test", L"Test", NULL);
+	SYSTEMTIME time;
+	GetLocalTime(&time);
+	printf("%4d-%2d-%2d", time.wYear, time.wMonth, time.wDay);
 }
