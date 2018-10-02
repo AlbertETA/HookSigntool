11c5fc2404c7dc81485cb049dde50c3d072bb920
Jemmy Wang <wangzihao12345678@126.com>
2018-10-02 11:40:50 +0800

    Read Config File

--- HookSigntool/main.cpp
+++ HookSigntool/main.cpp
@@ -1,11 +1,17 @@
 #pragma comment(lib, "detours.lib")
+
 #include <Windows.h>
 #include <wincrypt.h>
 #include <stdlib.h>
 #include <detours.h>
+#include <direct.h>
 
 using fntCertVerifyTimeValidity = decltype(CertVerifyTimeValidity);
 using fntGetLocalTime = decltype(GetLocalTime);
+
+int year = -1;
+int month = -1;
+int day = -1;
 
 fntCertVerifyTimeValidity *pOldCertVerifyTimeValidity = NULL;
 fntGetLocalTime *pOldGetLocalTime = NULL;
@@ -23,29 +29,43 @@ void WINAPI NewGetLocalTime(
 )
 {
 	pOldGetLocalTime(lpSystemTime);
-	lpSystemTime->wYear = 2011;
-	lpSystemTime->wMonth = 4;
-	lpSystemTime->wDay = 1;
+	if (year >= 0)
+		lpSystemTime->wYear = year;
+	if (month >= 0)
+		lpSystemTime->wMonth = month;
+	if (day >= 0)
+		lpSystemTime->wDay = day;
 }
 
 
 BOOL WINAPI DllMain(
 	_In_ HINSTANCE hinstDLL,
 	_In_ DWORD fdwReason,
-	_In_ LPVOID lpvReserved
-)
+	_In_ LPVOID lpvReserved)
 {
 	if (fdwReason == DLL_PROCESS_ATTACH)
 	{
+		//Get config file
+		char buf[260];
+		char* p = strstr(GetCommandLineA(), "-config ");
+		if (p) {
+			strcat_s(buf, p);
+		}
+		else {
+			_getcwd(buf, sizeof(buf));
+			strcat_s(buf, "\\hook.ini");
+		}
+		year = GetPrivateProfileInt("Time", "Year", -1, buf);
+		month = GetPrivateProfileInt("Time", "Month", -1, buf);
+		day = GetPrivateProfileInt("Time", "Day", -1, buf);
+
 		pOldCertVerifyTimeValidity = (fntCertVerifyTimeValidity *)GetProcAddress(LoadLibraryW(L"crypt32.dll"), "CertVerifyTimeValidity");
 		pOldGetLocalTime = (fntGetLocalTime *)GetProcAddress(LoadLibraryW(L"kernel32.dll"), "GetLocalTime");
 
-		MessageBoxA(NULL, "Attaching", "Hook", NULL);
 		DetourTransactionBegin();
 		DetourAttach(&(PVOID&)pOldCertVerifyTimeValidity, NewCertVerifyTimeValidity);
 		DetourAttach(&(PVOID&)pOldGetLocalTime, NewGetLocalTime);
 		DetourTransactionCommit();
-		MessageBoxA(NULL, "Attached", "Hook", NULL);
 	}
 	return 1;
 }
